f1095592967265b72609b488b6031c853a2d041d
tomaszzywicki <tomasz.zywicki03@gmail.com>
2025-11-09 23:21:43 +0100

    add docker templates

---
+++ backend/app/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+
+app = FastAPI(title='CalCones API',
+              description='API for CalCones app',
+              version="1.0.0")
+
+@app.get("/")
+async def read_root():
+    return {"message": "Hello root"}
