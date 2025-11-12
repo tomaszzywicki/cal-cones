950abcc1189a1ce191fc380d9192a7011b1a2f03
tomaszzywicki <tomasz.zywicki03@gmail.com>
2025-11-12 20:01:53 +0100

    frontend api client setup

---
+++ frontend/lib/core/network/api_client.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+import 'package:frontend/features/auth/services/firebase_auth_service.dart';
+import 'package:http/http.dart' as http;
+
+class ApiClient {
+  static const String baseUrl = '';
+  final FirebaseAuthService _firebaseAuthService;
+
+  ApiClient(this._firebaseAuthService);
+
+  Future<Map<String, String>> _getHeaders() async {
+    final token = await _firebaseAuthService.getIdToken();
+    return {
+      'Content-Type': 'application/json',
+      if (token != null) 'Authorization': 'Bearer $token',
+    };
+  }
+
+  Future<http.Response> get(String endpoint) async {
+    final headers = await _getHeaders();
+    return http.get(Uri.parse('$baseUrl$endpoint'), headers: headers);
+  }
+
+  Future<http.Response> post(String endpoint, Map<String, dynamic> body) async {
+    final headers = await _getHeaders();
+    return http.post(
+      Uri.parse('$baseUrl$endpoint'),
+      body: json.encode(body),
+      headers: headers,
+    );
+  }
+
+  Future<http.Response> put(String endpoint, Map<String, dynamic> body) async {
+    final headers = await _getHeaders();
+    return http.put(
+      Uri.parse('$baseUrl$endpoint'),
+      body: json.encode(body),
+      headers: headers,
+    );
+  }
+
+  Future<http.Response> delete(String endpoint) async {
+    final headers = await _getHeaders();
+    return http.delete(Uri.parse('$baseUrl$endpoint'), headers: headers);
+  }
+}
--- frontend/lib/features/auth/services/firebase_auth_service.dart
+++ frontend/lib/features/auth/services/firebase_auth_service.dart
@@ -3,9 +3,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 class FirebaseAuthService {
   final _firebaseAuth = FirebaseAuth.instance;
 
-  // void test() {
-  //   _firebaseAuth.signout
-  // }
+  Stream<User?> get userStream => _firebaseAuth.authStateChanges();
+
+  User? get currentUser => _firebaseAuth.currentUser;
 
   Future<UserCredential> signInWithEmailAndPassword(
     String email,
@@ -21,6 +21,11 @@ class FirebaseAuthService {
     return _firebaseAuth.signOut();
   }
 
-  Stream<User?> get userStream => _firebaseAuth.authStateChanges();
-  User? get currentUser => _firebaseAuth.currentUser;
+  Future<String?> getIdToken() async {
+    final User? currentUser = _firebaseAuth.currentUser;
+    if (currentUser == null) {
+      return null;
+    }
+    return currentUser.getIdToken();
+  }
 }
