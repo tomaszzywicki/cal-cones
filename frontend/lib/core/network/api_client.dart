e737d0b26285fcf6fdd1003942f3fb464246d5a5
tomaszzywicki <tomasz.zywicki03@gmail.com>
2025-11-24 22:19:30 +0100

    local changes before merge

---
+++ frontend/lib/core/network/api_client.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+import 'package:frontend/features/auth/services/firebase_auth_service.dart';
+import 'package:http/http.dart' as http;
+
+class ApiClient {
+  static const String baseUrl = 'https://bacon-weblogs-parallel-freely.trycloudflare.com';
+  final FirebaseAuthService _firebaseAuthService;
+
+  ApiClient(this._firebaseAuthService);
+
+  Future<Map<String, String>> _getHeaders() async {
+    final token = await _firebaseAuthService.currentUser!.getIdToken();
+    return {'Content-Type': 'application/json', if (token != null) 'Authorization': 'Bearer $token'};
+  }
+
+  Future<http.Response> get(String endpoint) async {
+    final headers = await _getHeaders();
+    return http.get(Uri.parse('$baseUrl$endpoint'), headers: headers);
+  }
+
+  Future<http.Response> post(String endpoint, Map<String, dynamic> body) async {
+    final headers = await _getHeaders();
+    return http.post(Uri.parse('$baseUrl$endpoint'), body: json.encode(body), headers: headers);
+  }
+
+  Future<http.Response> put(String endpoint, Map<String, dynamic> body) async {
+    final headers = await _getHeaders();
+    return http.put(Uri.parse('$baseUrl$endpoint'), body: json.encode(body), headers: headers);
+  }
+
+  Future<http.Response> delete(String endpoint) async {
+    final headers = await _getHeaders();
+    return http.delete(Uri.parse('$baseUrl$endpoint'), headers: headers);
+  }
+}
