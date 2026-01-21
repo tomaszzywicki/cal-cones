7c57d6b3d4c7054900ff4a5265cc2c3de52ab047
tomaszzywicki <tomasz.zywicki03@gmail.com>
2026-01-21 16:46:27 +0100

    fixes

--- frontend/lib/features/other/presentation/screens/user_info_page.dart
+++ frontend/lib/features/other/presentation/screens/user_info_page.dart
@@ -17,6 +17,7 @@ class UserInfo extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
+        scrolledUnderElevation: 0,
         elevation: 0,
         leading: IconButton(
           icon: const Icon(Icons.arrow_back, color: Colors.black),
